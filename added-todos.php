536ce798443de1bb705fb10fbaf7ef809185d522
test <abc@gmail.com>
2026-04-15 18:11:41 +0600

    latest commit

--- added-todos.php
+++ added-todos.php
@@ -3,18 +3,24 @@ include 'layouts/header.php';
 include_once './database/env.php';
 $query = "SELECT * FROM `todos`";
 $response = mysqli_query($connection, $query);
-
+$todos = mysqli_fetch_all($response, 1);
 ?>
 
     <div class="container my-5">
         <div class="row">
-            <div class="col-lg-4 mb-3">
+        <?php
+            foreach($todos as $todo) {
+            ?>
+                <div class="col-lg-4 mb-3">
                 <div class="card">
-                    <div class="card-header bg-dark text-white">Title</div>
-                    <div class="card-body bg-dark-subtle">Description</div>
-                    <div class="card-footer bg-secondary-subtle">Deadline</div>
+                    <div class="card-header bg-dark text-white"><?= $todo['title']; ?></div>
+                    <div class="card-body bg-dark-subtle"><?= $todo['description']; ?></div>
+                    <div class="card-footer bg-secondary-subtle"><?php echo $todo['deadline']; ?></div>
                 </div>
             </div>
+            <?php
+            }
+            ?>
         </div>
     </div>
     
